119e0c1c981dfd2c3d88e81e539619a72f1756d1
Mkdir <79910206+iTSiolinux@users.noreply.github.com>
2024-09-24 20:20:33 +0300

    Create basic.js

---
+++ basic.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Paper IO hack
+// @namespace    https://raw.githubusercontent.com/iTSiolinux/paper-io-conflict-HACK/refs/heads/main/basic.js
+// @version      2024-09-24
+// @description  try to take over the world!
+// @author       iTSiolinux
+// @match        https://paper-io.com/conflict/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=paper-io.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
